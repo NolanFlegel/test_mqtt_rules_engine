c548e13d1b044f10387d5a7f79d607abca79f768
Nolan Flegel <nolanflegel@outlook.com>
2024-11-29 13:05:14 -0800

    feat: added additional handling for edge cases

--- src/rule_engine.py
+++ src/rule_engine.py
@@ -30,7 +30,8 @@ def new_mqtt_client() -> mqtt_client:
 
     def on_message(client, userdata, message):
         output_message = json.dumps(rule_engine(message.payload))
-        response = client.publish(topic=MQTT_OUTPUT_TOPIC, payload=output_message)
+        if output_message is not None:
+            response = client.publish(topic=MQTT_OUTPUT_TOPIC, payload=output_message)
 
     def on_subscribe(client, userdata, mid, reason_codes, properties):
         print(f"Subscribed with reason code {reason_codes}")
@@ -49,22 +50,31 @@ def new_mqtt_client() -> mqtt_client:
 
 def rule_engine(message):
     print(f"message payload: {message}")
+    required_keys = ["id", "familyUnitInPayForDecember", "numberOfChildren", "familyComposition"]
     message_body = json.loads(message)
+
+    # If any keys are missing or id is empty return without publishing
+    # could expand this to cover cases where any dependent field is empty
+    if not set(required_keys).issubset(message_body):
+        print(f"message body missing required keys")
+        return None
+    
+    if message_body['id'] is None or message_body['id'] == "":
+        return None
+    
+    if message_body == None or message_body == {}:
+        print(f"Message body empty, return")
+        return None
+    
     default_output_message = {
-        "id": message_body["id"] if message_body["id"] else None, 
+        "id": message_body["id"], 
         "isEligible": False,
         "baseAmount": 0.0,
         "childrenAmount": 0.0,
         "supplementAmount": 0.0
         }
 
-    # If message body is empty return without publishing
-    # could expand this to cover cases where any dependent field is empty
-    if message_body == None or message_body == {}:
-        print(f"Message body empty, return")
-        return default_output_message
-    
-    # loading default values
+    # set default values
     is_eligible = message_body["familyUnitInPayForDecember"]
     number_of_children = int(message_body["numberOfChildren"])
     family_composition = message_body["familyComposition"]
@@ -75,6 +85,10 @@ def rule_engine(message):
     if is_eligible == False:
         return default_output_message
     
+    # handle edge case of invalid data
+    if number_of_children < 0 or family_composition not in FAMILY_COMPOSITION:
+        return default_output_message
+
     if is_eligible and number_of_children > 0:
         base_amount = 120.00
         children_amount = 20.00 * number_of_children
@@ -82,10 +96,12 @@ def rule_engine(message):
         base_amount = 120.00
     elif is_eligible and family_composition == FAMILY_COMPOSITION["single"]:
         base_amount = 60.00
+    else:
+        is_eligible = False
 
     supplement_amount = base_amount + children_amount
     output_message = {
-        "id": message_body["id"] if message_body["id"] else None, 
+        "id": message_body["id"], 
         "isEligible": is_eligible,
         "baseAmount": base_amount,
         "childrenAmount": children_amount,
